38a471df9b05c5e16e4185bbccc5ff72e6165c53
Kevin Hansen <kevin.lhansen@hotmail.com>
2020-11-16 14:28:29 +0100

    Add assertion tests for CutAction, PasteAction, CopyAction classes

--- JHotDraw/src/main/java/org/jhotdraw/app/action/CopyAction.java
+++ JHotDraw/src/main/java/org/jhotdraw/app/action/CopyAction.java
@@ -11,7 +11,6 @@
  * accordance with the license agreement you entered into with  
  * the copyright holders. For details see accompanying license terms. 
  */
-
 package org.jhotdraw.app.action;
 
 import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
@@ -20,20 +19,24 @@ import java.awt.event.*;
 import javax.swing.*;
 import org.jhotdraw.app.JHotDrawFeatures;
 import org.jhotdraw.util.*;
+
 /**
  * Copies the selected region and place its contents into the system clipboard.
- * Acts on the EditableComponent or JTextComponent which had the focus when
- * the ActionEvent was generated.
+ * Acts on the EditableComponent or JTextComponent which had the focus when the
+ * ActionEvent was generated.
  *
  * @author Werner Randelshofer
- * @version 2.0 2007-04-13 Use javax.swing.TransferHandler instead of 
- * interface EditableComponent. 
+ * @version 2.0 2007-04-13 Use javax.swing.TransferHandler instead of interface
+ * EditableComponent.
  * <br>1.0 October 9, 2005 Created.
  */
 public class CopyAction extends AbstractAction {
+
     public final static String ID = "edit.copy";
-    
-    /** Creates a new instance. */
+
+    /**
+     * Creates a new instance.
+     */
     public CopyAction() {
         ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.app.Labels");
         labels.configureAction(this, ID);
@@ -41,17 +44,18 @@ public class CopyAction extends AbstractAction {
 
     @FeatureEntryPoint(JHotDrawFeatures.BASIC_EDITING)
     @Override
-   public void actionPerformed(ActionEvent evt) {
+    public void actionPerformed(ActionEvent evt) {
         Component focusOwner = KeyboardFocusManager.
                 getCurrentKeyboardFocusManager().
                 getPermanentFocusOwner();
-        if (focusOwner != null && focusOwner instanceof JComponent) {
+        assert focusOwner != null;
+        if (focusOwner instanceof JComponent) {
             JComponent component = (JComponent) focusOwner;
             component.getTransferHandler().exportToClipboard(
                     component,
                     component.getToolkit().getSystemClipboard(),
                     TransferHandler.COPY
-                    );
+            );
         }
     }
 }
